ee10690920662611a9f3a276b2b4d96b3bf687f6
edean78 <edean78@gmail.com>
2019-12-10 21:52:34 -0500

    deleted isAuthenticated variable

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
