0ca44588996858948f78144f40f75b610028c38d
edean78 <edean78@gmail.com>
2019-12-12 11:52:22 -0500

    made minor changes to server.js, apiRoutes, list.html, members.js, and config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 require('dotenv').config(); // this is important!
 
 module.exports = {
-"development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": 3307,
-    "dialect": "mysql"
-}}
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": 3306,
+        "dialect": "mysql"
+    }
+}
