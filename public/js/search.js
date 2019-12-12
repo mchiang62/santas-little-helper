6791c31d7d05d5448a7004fa21bc78d4ae665cfb
Melissa Chiang <mchiang62@gmail.com>
2019-12-12 17:42:28 -0500

    updates on search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,17 +1,44 @@
 $(document).ready(function () {
 
+    var firstName = $("#search-first");
+    var lastName = $("#search-last");
 
     $("#search-button").on("click", function (event) {
         event.preventDefault();
-        $('#displayFirst').text($("#search-first").val());
-        $('#displayLast').text($("#search-last").val());
 
-    //   still need to grab data from the wish list
+        var searchName = {
+
+            firstName: firstName.val().trim(),
+            lastName: lastName.val().trim(),
+
+        };
+        console.log(searchName + "search");
+
+        searchWishlist(searchName);
+
+        function searchWishlist(Get) {
+
+            $.get("/api/wishlists", Get, function (data) {
+
+                $('#displayFirst').text(data);
+                $('#displayLast').text(data);
+
+            });
+
+
+
+
+        }
+
+        //     $('#displayFirst').text($("#search-first").val());
+        //     $('#displayLast').text($("#search-last").val());
+
+        // //   still need to grab data from the wish list
 
     });
 
 
-    
+
 
 
 });
