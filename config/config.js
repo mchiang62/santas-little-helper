331354e407e5f740a22e57c8b324cb2536d28867
Melissa Chiang <mchiang62@gmail.com>
2019-12-14 14:20:30 -0500

    udates config once again

--- config/config.js
+++ config/config.js
@@ -11,41 +11,11 @@ module.exports = {
     },
 
     "production": {
-        "username": "lg33dfs017hlqrz3",
-        "password": "s9cc4b7h1xfupr5d",
-        "database": "c6qhxrwo1qa3bx74",
-        "host": process.env.DB_HOST,
-        "port": 3306,
+        "username": process.env.USERNAME,
+        "password": process.env.PASSWORD,
+        "database": process.env.JAWSDB_URL,
+        "host": process.env.HOST,
         "dialect": "mysql",
-        "use_env_variable": process.env.JAWSDB_URL
 
     }
 };
-
-
-// var mysql = require('mysql');
-// var connection;
-
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         // "username": process.env.DB_USERNAME,
-//         // "password": process.env.DB_PASSWORD,
-//         // "database": process.env.DB_DATABASE,
-//         // "host": process.env.DB_HOST,
-//         // "port": 3307,
-//         // "dialect": "mysql"
-
-//     });
-// };
-
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error("error connecting: " + err.message)
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId)
-// });
-
-// module.exports = connection;
