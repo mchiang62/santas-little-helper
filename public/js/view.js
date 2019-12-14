2a252e684a8cc613420e74efa78bc5d4a30980cd
Dan Gallagher <dagallagher221@gmail.com>
2019-12-14 01:06:31 -0500

    got delete to work

--- public/js/view.js
+++ public/js/view.js
@@ -1,3 +1,27 @@
+function deleteList(id) {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/wishlists/" + id
+  })
+    .then(function() {
+      console.log("We did a thing");
+      //getPosts(postCategorySelect.val());
+    });
+}
+
+function deleteItem(id) {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/wishlistitem/" + id
+  })
+    .then(function() {
+      console.log("We did a thing");
+      //getPosts(postCategorySelect.val());
+    });
+}
+
+
+
 $.get("/api/wishlists", function(data) {
     if (data.length !== 0) {
   
@@ -5,17 +29,35 @@ $.get("/api/wishlists", function(data) {
   
         var row = $("<div>");
      
-          var deleteBtn = $("<button>");
-          deleteBtn.text("x");
-          deleteBtn.addClass("delete btn btn-danger");
-        row.append(deleteBtn)
-        row.append("<h2>" + data[i].name + "</h2>");
+          var deleteBtnList = $("<button>");
+          deleteBtnList.text("x");
+          deleteBtnList.addClass("delete btn btn-danger delete-list");
+          deleteBtnList.attr("id", "deleteList" + data[i].id);
+          var x73 = data[i].id;
+          
+
+        row.append('<h2>' + data[i].name + "            " + deleteBtnList.prop('outerHTML') + "</h2>");
+        
+        $("body").on("click", ("#deleteList" + data[i].id), (event) => {
+          
+          deleteList(event.target.id.split("deleteList")[1]);
+      
+        });
         
 
         for (var j = 0; j < data[i].Items.length; j++) {
+
+          var deleteBtnItem = $("<button>");
+          deleteBtnItem.text("x");
+          deleteBtnItem.addClass("delete btn btn-danger delete-item");
+          deleteBtnItem.attr("id", "deleteItem" + data[i].Items[j].id);
+
+          row.append("<p>"+ data[i].Items[j].item + "," + " " + "$" + data[i].Items[j].price +  "    " + deleteBtnItem.prop('outerHTML') +  "</p>");
+          $("body").on("click", ("#deleteItem" + data[i].Items[j].id), (event) => {
           
-          row.append("<p>"+ data[i].Items[j].item + "," + " " + "$" + data[i].Items[j].price + "<p>");
-   
+          deleteItem(event.target.id.split("deleteItem")[1]);
+          
+        });
         }
         $(".card-text").prepend(row);
       }
