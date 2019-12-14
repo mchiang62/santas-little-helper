7d457ba5870e6602193f9cc92cb97b0b60ec31af
Melissa Chiang <mchiang62@gmail.com>
2019-12-14 13:12:06 -0500

    trying to get heroku to work

--- config/config.js
+++ config/config.js
@@ -1,11 +1,44 @@
 require('dotenv').config(); // this is important!
 
 module.exports = {
-"development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": 3307,
-    "dialect": "mysql"
-}}
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": 3307,
+        "dialect": "mysql"
+    },
+
+    "production": {
+        // "username": process.env.DB_USERNAME,
+        // "password": process.env.DB_PASSWORD,
+        // "database": process.env.DB_DATABASE,
+        // "host": process.env.DB_HOST,
+        // "port": 3307,
+        "dialect": "mysql",
+        "use_env_variable": process.env.JAWSDB_URL
+
+    }
+};
+
+
+// var mysql = require('mysql');
+// var connection;
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection ({
+//         "username": process.env.DB_USERNAME,
+//         "password": process.env.DB_PASSWORD,
+//         "database": process.env.DB_DATABASE,
+//         "host": process.env.DB_HOST,
+//         "port": 3307,
+//         "dialect": "mysql"
+
+//     });
+// };
+
+// connection.connect();
+// module.exports = connection;
