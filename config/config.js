b07511715e38b327ca6e889458b23bf74db33e77
Melissa Chiang <mchiang62@gmail.com>
2019-12-14 14:25:19 -0500

    updates database var

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = {
         "username": process.env.USERNAME,
         "password": process.env.PASSWORD,
         "database": process.env.DATABASE,
-        "host": process.env.HOST,
+        "host": process.env.JAWSDB_URL,
         "dialect": "mysql",
 
     }
